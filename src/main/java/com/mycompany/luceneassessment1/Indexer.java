b53fcd7580ba4f1c007c94ca97d8e8dbda16e805
Karina <Karina@10.6.38.191>
2021-10-21 17:45:43 +0100

    init commit

---
+++ src/main/java/com/mycompany/luceneassessment1/Indexer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.luceneassessment1;
+
+/**
+ *
+ * @author Karina
+ */
+public class Indexer {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
