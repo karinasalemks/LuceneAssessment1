43392d7c7df2026d7cee799785919c8887791a79
Karina <Karina@10.6.38.191>
2021-10-23 17:31:35 +0100

    test args

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 mvn clean install
 mvn compile
-mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile"
+mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile" \
+  -Dexec.args="First Second"
 cd trec_eval
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM.txt
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBM25.txt
--- src/main/java/com/mycompany/luceneassessment1/MainFile.java
+++ src/main/java/com/mycompany/luceneassessment1/MainFile.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) throws IOException {
+        System.out.println(args[0]);
         Indexer indexer = new Indexer();
         Inquierer inquierer = new Inquierer();
         indexer.indexDoc();
