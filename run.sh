0004d1ac01f69d1db99f44ba02bff1a1d4c7a2f9
Karina <Karina@10.6.38.191>
2021-10-23 19:29:11 +0100

    add echo

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile"
 cd trec_eval
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_SA.txt
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_SA.txt
+echo "English Analyzer"
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_EA.txt
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_EA.txt
 
