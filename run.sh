7471caefb904ab64e1e227fb7eddb22810985e0f
Karina <Karina@10.6.38.191>
2021-10-23 18:19:45 +0100

    fix shell

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile" \
   -Dexec.args="$i"
 cd trec_eval
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/results.txt
+cd ..
 done
 
