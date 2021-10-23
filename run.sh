d575e76844ea46e303d1bca06d81a5541b6153bc
Karina <Karina@10.6.38.191>
2021-10-23 18:07:15 +0100

    fix shell

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mvn compile
 for i in 1 2
 do
 mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile" \
-  -Dexec.args="$searcheri"
+  -Dexec.args="$i"
 cd trec_eval
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/results.txt
 done
