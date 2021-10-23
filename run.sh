65ef89d577cb6fb0c20ae91f2c604b2c6e0043cf
Karina <Karina@10.6.38.191>
2021-10-23 19:50:53 +0100

    add cd commans

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 mvn clean install
 mvn compile
 mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile" 
-# cd trec_eval
+cd trec_eval
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_SA.txt
 ./trec_eval -m runid -m map -m gm_map -m P.5 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_SA.txt
 echo "English Analyzer"
