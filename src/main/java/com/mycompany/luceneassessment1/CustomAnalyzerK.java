f63759661ebf0e7bd99619ecd174238931523835
Karina <Karina@10.6.38.191>
2021-10-24 10:15:51 +0100

    changes to analyzer

--- src/main/java/com/mycompany/luceneassessment1/CustomAnalyzerK.java
+++ src/main/java/com/mycompany/luceneassessment1/CustomAnalyzerK.java
@@ -8,7 +8,7 @@ import org.apache.lucene.analysis.custom.CustomAnalyzer;
 public class CustomAnalyzerK {
     public Analyzer createCustomAnalyzer() throws IOException {
         Analyzer analyzer = CustomAnalyzer.builder()
-          .withTokenizer("icu")  
+          .withTokenizer("classic")  
           .addTokenFilter("lowercase")  
           .addTokenFilter("asciiFolding")  
           .addTokenFilter("stop")
