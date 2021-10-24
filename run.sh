24510ea0849a782af407b36cbfdbc670669046bd
Karina <Karina@10.6.38.191>
2021-10-24 17:33:07 +0100

    commit num_ret

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@ mvn compile
 mvn exec:java -Dexec.mainClass="com.mycompany.luceneassessment1.MainFile" 
 cd trec_eval
 echo "############## Whitespace Analyzer ##############"
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Witespace.txt
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Whitespace.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Witespace.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Whitespace.txt
 echo "############## Simple Analyzer ##############"
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Simple.txt
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Simple.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Simple.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Simple.txt
 echo "############## Standard Analyzer ##############"
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Standard.txt
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Standard.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Standard.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Standard.txt
 echo "############## English Analyzer ##############"
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_English.txt
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_English.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_English.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_English.txt
 echo "############## Karina's custom Analyzer ##############"
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Karina.txt
-./trec_eval -m runid -m map -m gm_map -m num_q -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Karina.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsVSM_Karina.txt
+./trec_eval -m runid -m map -m gm_map -m num_q -m num-ret -m num_rel -m num_rel_ret -m Rprec -m P.5,10 ../src/cran/QRelsCorrectedforTRECeval ../results/resultsBMS25_Karina.txt
